055e75c82718b84a927a1754974f494e825cd2c4
quangvh <vhquangbk307@gmail.com>
2013-06-10 15:55:16 +0700

    step 4 - clean

--- StringCalculator11/src/main/java/Calculator.java
+++ StringCalculator11/src/main/java/Calculator.java
@@ -6,18 +6,20 @@ public class Calculator {
     public static int add(String text) throws Exception {
         int sum = 0;
         String strDelimiter = "";
+        String strNumber = "";
         if (text.isEmpty()) {
             return 0;
         } else {
             if (text.startsWith("//")) {
                 strDelimiter = text.substring(2, 3) + "|\n";
-                text = text.substring(2);
+                strNumber = text.substring(2);
             } else if (text.contains(",")){
                 strDelimiter = ",|\n";
+                strNumber = text;
             } else {
                 return Integer.parseInt(text);
             }
-            String[] listNumber = text.split(strDelimiter);
+            String[] listNumber = strNumber.split(strDelimiter);
             for (String number : listNumber) {
                 if (!number.equals(""))
                     sum += Integer.parseInt(number);
