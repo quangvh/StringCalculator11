03ea2cf1c29bfe2f90eba0cda5c24c8ae6bc4312
quangvh <vhquangbk307@gmail.com>
2013-06-10 15:50:42 +0700

    step 4 - green

--- StringCalculator11/src/main/java/Calculator.java
+++ StringCalculator11/src/main/java/Calculator.java
@@ -5,17 +5,22 @@
 public class Calculator {
     public static int add(String text) throws Exception {
         int sum = 0;
+        String strDelimiter = "";
         if (text.isEmpty()) {
             return 0;
         } else {
-            if (text.contains(",")){
-                String strDelimiter = ",|\n";
-                String[] listNumber = text.split(strDelimiter);
-                for (String number : listNumber) {
-                    sum += Integer.parseInt(number);
-                }
+            if (text.startsWith("//")) {
+                strDelimiter = text.substring(2, 3) + "|\n";
+                text = text.substring(2);
+            } else if (text.contains(",")){
+                strDelimiter = ",|\n";
             } else {
                 return Integer.parseInt(text);
+            }
+            String[] listNumber = text.split(strDelimiter);
+            for (String number : listNumber) {
+                if (!number.equals(""))
+                    sum += Integer.parseInt(number);
             }
         }
         return sum;
